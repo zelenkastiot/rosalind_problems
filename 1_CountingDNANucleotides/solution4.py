7c3bfefd91bf039a7210d9ce2d943f4df1eeedf7
Kiril Zelenkovski <zelenkastiot@gmail.com>
2021-02-12 11:29:00 +0100

    Add codes for problems 2-7

---
+++ 1_CountingDNANucleotides/solution4.py
@@ -0,0 +1,14 @@
+"""
+
+ Created on 12-Feb-21
+ @author: Kiril Zelenkovski
+ @source: Reproducible Bioinformatics - O'Reilly
+
+ solution4.py:
+
+"""
+
+with open('rosalind_dna.txt', 'r') as file:
+    dna = file.read().replace('\n', '')
+
+
